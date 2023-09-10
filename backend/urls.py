f1ed501949b67066bec8478c16f31edc47b08ba2
Your Name <keefevuong123@gmail.com>
2023-09-11 07:45:59 +1000

    Added imports

--- backend/urls.py
+++ backend/urls.py
@@ -20,6 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from tiktok.views import TiktokListApiView, WeeklyReportListApiView, WeeklyReportApiView, TiktokApiView, UserApiView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
