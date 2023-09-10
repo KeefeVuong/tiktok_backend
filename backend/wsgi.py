9f3d6e1d837e3982caa76ae3b5743f15baccf97e
Your Name <keefevuong123@gmail.com>
2023-09-11 01:24:49 +1000

    test

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,10 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
+app = application
 # import os
 # from django.core.wsgi import get_wsgi_application
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
