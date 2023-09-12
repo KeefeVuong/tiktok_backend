446ae1ad80ce54427c33a92148b4085223690d01
Your Name <keefevuong123@gmail.com>
2023-09-12 17:57:46 +1000

    Changed to apify to fetch data

--- tiktok/models.py
+++ tiktok/models.py
@@ -1,8 +1,9 @@
 from djongo import models
+import uuid
 from django.contrib.auth.models import User
 
 class WeeklyReport(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
