c4154ff0e48b07d6a2c3a6109036af7bbe4f3693
Keefe Vuong <z5360545@ad.unsw.edu.au>
2023-09-19 19:47:30 +1000

    test

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['keefe-tk-be.xyz', 'www.keefe-tk-be.xyz','192.9.172.61']
+ALLOWED_HOSTS = ['keefe-tk-be.xyz', 'www.keefe-tk-be.xyz','192.9.172.61', '127.0.0.1']
 
 
 # Application definition
